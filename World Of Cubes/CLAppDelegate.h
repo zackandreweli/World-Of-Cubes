5a5860f5d3af6f25f639c13a7ee680500e901cf0
Andrew B <zackandreweli@gmail.com>
2012-03-07 18:02:22 -0500

    Initial Commit

---
+++ World Of Cubes/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  World Of Cubes
+//
+//  Created by Andrew B on 3/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ World Of Cubes/CLAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  CLAppDelegate.m
+//  World Of Cubes
+//
+//  Created by Andrew B on 3/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CLAppDelegate.h"
+
+@implementation CLAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
